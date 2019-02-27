599fc94bb1177093248cb5a665acaaf67cdce2c6
Rafa p.s <rafaelpassos.web@gmail.com>
2019-02-27 01:08:28 -0300

    Nova Atualização

---
+++ SMC/Controllers/PainelController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SMC.Controllers
+{
+    public class PainelController : Controller
+    {
+        // GET: Painel
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Menu()
+        {
+            return PartialView();
+        }
+    }
+}
