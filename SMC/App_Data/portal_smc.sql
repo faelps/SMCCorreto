4ddfc9932e78be5021feccce2209a3e4cbe7dc0f
Felipe "knt" Cardoso <felipe_cardoso_007@hotmail.com>
2019-03-06 17:18:18 -0400

    Adicionado banco e conexao ao banco

---
+++ SMC/App_Data/portal_smc.sql
@@ -0,0 +1,44 @@
+-- Table: public.dados_pessoais
+
+-- DROP TABLE public.dados_pessoais;
+
+CREATE TABLE public.dados_pessoais
+(
+    id integer NOT NULL,
+    nome character varying(300) COLLATE pg_catalog."default" NOT NULL,
+    cpf character varying(11) COLLATE pg_catalog."default" NOT NULL,
+    telefone character varying(11) COLLATE pg_catalog."default",
+    email character varying(100) COLLATE pg_catalog."default",
+    endereco character varying(300) COLLATE pg_catalog."default",
+    CONSTRAINT pk_id PRIMARY KEY (id)
+)
+WITH (
+    OIDS = FALSE
+)
+TABLESPACE pg_default;
+
+ALTER TABLE public.dados_pessoais
+    OWNER to postgres;
+
+
+
+-- Table: public.autenticacao
+
+-- DROP TABLE public.autenticacao;
+
+CREATE TABLE public.autenticacao
+(
+    id integer NOT NULL,
+    id_dados_pessoais integer NOT NULL,
+    login character varying(80) COLLATE pg_catalog."default" NOT NULL,
+    senha character varying(80) COLLATE pg_catalog."default" NOT NULL,
+    tipo_usuario integer,
+    CONSTRAINT pk_autenticao PRIMARY KEY (id)
+)
+WITH (
+    OIDS = FALSE
+)
+TABLESPACE pg_default;
+
+ALTER TABLE public.autenticacao
+    OWNER to postgres;
---
+++ SMC/Repositorio/Conexao.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using Npgsql;
+using System.Configuration;
+
+namespace SMC.Repositorio
+{
+    public class Conexao
+    {
+       
+
+        //Conexao ao Banco
+        public static NpgsqlConnection Conectar() {
+            //parametros de conexao ao banco
+            string conexString = ConfigurationManager.ConnectionStrings["ConexaoBanco"].ConnectionString;
+            //Instanciando nova conexao
+            NpgsqlConnection con = new NpgsqlConnection(conexString);
+            //Abrindo conexao
+            con.Open();
+            return con;
+        }
+        //Comando CRUD pré-definido
+        public static void CRUD(NpgsqlCommand cmd) {
+            //Abre nova conexao
+            NpgsqlConnection con = Conectar();
+            cmd.Connection = con;
+            //Executa a query
+            cmd.ExecuteNonQuery();
+            //Fecha conexao
+            con.Close();
+        }
+
+        //Comando generico de select
+        public static NpgsqlDataReader Selecionar(NpgsqlCommand cmd) {
+            NpgsqlConnection con = Conectar();
+            cmd.Connection = con;
+            NpgsqlDataReader dr = cmd.ExecuteReader(CommandBehavior.CloseConnection);
+            return dr;
+        }
+
+    }
+}
